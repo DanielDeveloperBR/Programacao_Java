91f0cde91991496158ff079b545eba969ef60bb5
Daniel <115842725+DanielDeveloperBR@users.noreply.github.com>
2023-03-13 19:37:24 -0300

    inicio

---
+++ baseJava/src/basejava/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basejava;
+
+/**
+ *
+ * @author 169127512022.5
+ */
+public class Principal {
+
+    
+    public static void main(String[] args) {
+       
+    //   int valor = 3;
+     //  if (valor % 2 == 0){
+    //       System.out.print("Par");
+    //  }else{
+   //        System.err.print("Impar");
+  //     }
+       Numero numero = new Numero();
+        numero.valor = 4;
+        System.out.print(numero.parInpar());
+    }
+    
+    
+    
+}
+   
